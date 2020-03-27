35eb1f4104d62e2a1a4ab21553e63c841bdc2a2f
Pau Larrea <paularrea12@hotmail.com>
2020-03-27 12:51:13 +0100

    hjkasdf

---
+++ Swap it/Swap it/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swap it
+//
+//  Created by mac on 27/03/2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
